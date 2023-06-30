ceee99d0eeccdea480c902b9a969331126a67135
RastkoAmun <raciosh@gmail.com>
2023-06-29 18:22:56 -0700

    feat: Setting up the template for main page
    Also adding new route to this main page

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Box, Button, Grid, Paper, Typography } from '@mui/material'
+
+const paperStyle = {
+  bgcolor: 'lightgray',
+  p: 2,
+}
+
+const buttonStyle = {
+  p: 0,
+  bgcolor: 'white',
+  color: 'black',
+  border: '1px solid black',
+  borderRadius: 1
+}
 
 const MainPage = () => {
   return (
-    <Typography variant='h1'>
-      Main Page
-    </Typography>
+    <Box>
+      <Grid container spacing={5} >
+        <Grid item xs={8}>
+          <Paper sx={paperStyle}>
+            <Typography> Statistical Data </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+        <Paper sx={paperStyle}>
+          <Box display='flex' justifyContent='space-between'>
+            <Typography variant='h6'> Profile </Typography>
+            <Button variant='contained' sx={buttonStyle}>Edit</Button>
+          </Box>
+          <Typography>Name</Typography>
+          <Typography>Height</Typography>
+          <Typography>Weight</Typography>
+        </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper sx={paperStyle}>
+            <Typography>
+              Logs
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
