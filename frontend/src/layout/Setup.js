39878c8d1d7c9fa4fc963f0cd009670f1be3dec5
00sea <seanshen12@gmail.com>
2023-06-29 20:23:08 -0700

    Setup.js modifications

--- frontend/src/layout/Setup.js
+++ frontend/src/layout/Setup.js
@@ -13,15 +13,15 @@ const Setup = () => {
     /* This is here to prevent users from using the ArrowForwardIcon button to move to next card
      when they haven't entered the required info. */
 
-    const handleNextPage = () => {
-      setCurrentPage(currentPage + 1);
-      if (frontPage===currentPage) {
-        setFrontPage(frontPage + 1);
-      }
-    };
-    const handlePrevPage = () => {
-      setCurrentPage(currentPage - 1);
-    };
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+    if (frontPage===currentPage) {
+      setFrontPage(frontPage + 1);
+    }
+  };
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
   
 
   // Keeps track of all the user's input information, passed to cardContent
@@ -34,19 +34,19 @@ const Setup = () => {
   switch (currentPage) {
     case 1:
       cardContent = (
-          <Gender selectedGender={selectedGender} setSelectedGender={setSelectedGender} handleNextPage={handleNextPage}/>
+        <Gender selectedGender={selectedGender} setSelectedGender={setSelectedGender} handleNextPage={handleNextPage}/>
       );
       break;
     case 2:
       cardContent = (
-          <Age selectedAge={selectedAge} setSelectedAge={setSelectedAge} handleNextPage={handleNextPage}/>
+        <Age selectedAge={selectedAge} setSelectedAge={setSelectedAge} handleNextPage={handleNextPage}/>
       );
       break;
     case 3:
-        cardContent = (
-          <Weight />
-        );
-        break;
+      cardContent = (
+        <Weight />
+      );
+      break;
     case 4:
       cardContent = (
         <Height />
@@ -54,45 +54,42 @@ const Setup = () => {
       break;
     default:
       cardContent = null;
+  };
+
+  const backGround = {
+    backgroundColor: 'white',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+  const cardBox = {
+    bgcolor: 'white',
+    borderRadius: '8px',
+    width: '65vw',
+    height: '75vh',
+    boxShadow: '0px 0px 50px 0px rgba(0,0,0,0.25)'
   }
 
-
-
-/*
-    const [selectedGender, setSelectedGender] = useState(null);
-    const [isNextButtonActive, setIsNextButtonActive] = useState(false);
-
-    const handleGenderSelection = (gender) => {
-        // Handles gender selection, activates next button
-        setSelectedGender(gender);
-        setIsNextButtonActive(true);
-    };
-
-    const handleNextButtonClick = () => {
-        // Handle next button click logic here
-        console.log('Next button clicked');
-    };
-*/
-
-    return (
-        <div style={{ backgroundColor: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Box sx={{ bgcolor: 'white', p: 3, borderRadius: '8px', width: '65vw', height: '75vh', padding: '0px', boxShadow: '0px 0px 50px 0px rgba(0,0,0,0.25)'}}>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                {/* Container for the progress bar + forwards and backwards arrow buttons */}
-                    <IconButton onClick={handlePrevPage} disabled={currentPage===1} color="primary" aria-label="back">
-                        <ArrowBackIcon />
-                    </IconButton>
-                    <IconButton onClick={handleNextPage} disabled={frontPage===currentPage} color="primary" aria-label="forward">
-                        <ArrowForwardIcon />
-                    </IconButton>
-                </Box>
-                <Box sx={{ display: 'flex', flexDirection:'column', justifyContent:'center', height:'80%' }}>
-                {/* This container holds the different card contents */}
-                  {cardContent}
-                </Box>
-            </Box>
-        </div>
-    );
+  return (
+    <div style={backGround}>
+      <Box style={cardBox}>
+        <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* Container for the progress bar + forwards and backwards arrow buttons */}
+          <IconButton onClick={handlePrevPage} disabled={currentPage===1} color="primary" aria-label="back">
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton onClick={handleNextPage} disabled={frontPage===currentPage} color="primary" aria-label="forward">
+            <ArrowForwardIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection:'column', justifyContent:'center', height:'80%' }}>
+        {/* This container holds the different card contents */}
+          {cardContent}
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Setup;
