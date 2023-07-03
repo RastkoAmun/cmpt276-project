b270fb77e8e511e038d95f20a8b0816f1652cf07
Anson Chan <achan436@my.bcit.ca>
2023-07-02 20:38:23 -0700

    chore: cleanup unused imports and one test endpoint

--- backend/src/main/java/com/cmpt276project/projectbackend/models/User.java
+++ backend/src/main/java/com/cmpt276project/projectbackend/models/User.java
@@ -1,6 +1,17 @@
 package com.cmpt276project.projectbackend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int uid;
   private String username;
   private String password;
   private String email;
