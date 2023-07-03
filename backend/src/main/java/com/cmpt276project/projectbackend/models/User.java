bc48d8a103e3c5eedd22db71bfd1d83aa8c21a71
Anson Chan <achan436@my.bcit.ca>
2023-07-02 17:18:50 -0700

    feat: simple user class

---
+++ backend/src/main/java/com/cmpt276project/projectbackend/models/User.java
@@ -0,0 +1,41 @@
+package com.cmpt276project.projectbackend.models;
+
+public class User {
+  private String username;
+  private String password;
+  private String email;
+
+  public User() {
+
+  }
+
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
