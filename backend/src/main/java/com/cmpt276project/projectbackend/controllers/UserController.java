da95d8f4842ef08a6343fb37b298a10ad0902814
Anson Chan <achan436@my.bcit.ca>
2023-07-02 20:29:13 -0700

    feat: http status codes added

--- backend/src/main/java/com/cmpt276project/projectbackend/controllers/UserController.java
+++ backend/src/main/java/com/cmpt276project/projectbackend/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.cmpt276project.projectbackend.controllers;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.cmpt276project.projectbackend.customerrors.UserError;
 import com.cmpt276project.projectbackend.models.User;
 import com.cmpt276project.projectbackend.models.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class UserController {
@@ -16,11 +18,24 @@ public class UserController {
   record UserRequest(String username, String password, String email) {
   }
 
+  record AdminRequest(String username, String adminKey) {
+  }
+
   private User testUser = new User("testUsername", "testEmail", "testPassword");
 
   @GetMapping("/test")
   public User test() {
     return testUser;
+  }
+
+  @DeleteMapping("/delete")
+  public void delete(@RequestBody AdminRequest request) {
+    User user = userRepo.findByUsername(request.username());
+    String admin = request.adminKey();
+
+    if (admin.equals("admin123")) {
+      userRepo.delete(user);
+    }
   }
 
   @PostMapping("/register")
@@ -47,18 +62,21 @@ public class UserController {
   }
 
   @PostMapping("/login")
-  public User login(@RequestBody UserRequest request) {
+  public User login(@RequestBody UserRequest request, HttpServletResponse res) throws IOException {
     String username = request.username();
     String password = request.password();
 
     User user = userRepo.findByUsername(username);
 
     if (user == null) {
-      return new UserError("User does not exist");
+      res.sendError(400, "User does not exist");
+      return null;
     }
 
     if (!password.equals(user.getPassword())) {
-      return new UserError("User password is incorrect");
+      res.sendError(401, "User password is wrong");
+      // return new UserError("User password is incorrect");
+      return null;
     }
 
     return user;
