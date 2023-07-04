0a5d0a0dccb25c1515bd87426cc701fc002b3967
Anson Chan <achan436@my.bcit.ca>
2023-07-03 23:34:41 -0700

    feat: added UID getter for User model

--- backend/src/main/java/com/cmpt276project/projectbackend/controllers/UserController.java
+++ backend/src/main/java/com/cmpt276project/projectbackend/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.cmpt276project.projectbackend.controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
