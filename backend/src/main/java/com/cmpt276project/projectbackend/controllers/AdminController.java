7a1a30eba95d466fd10d7b3e24fb79ed022a4d11
Anson Chan <achan436@my.bcit.ca>
2023-07-14 13:44:48 -0700

    feat: basic admin dash tools

--- backend/src/main/java/com/cmpt276project/projectbackend/controllers/AdminController.java
+++ backend/src/main/java/com/cmpt276project/projectbackend/controllers/AdminController.java
@@ -2,16 +2,13 @@ package com.cmpt276project.projectbackend.controllers;
 
 import java.io.IOException;
 
-import org.hibernate.annotations.Cascade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.cmpt276project.projectbackend.models.User;
 import com.cmpt276project.projectbackend.models.UserRepository;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 // @CrossOrigin(origins = { "http://localhost:3000" }, allowedHeaders = "*", allowCredentials = "true")
 @RestController
@@ -21,7 +18,7 @@ public class AdminController {
   @Autowired
   private UserRepository userRepo;
 
-  record AdminRequest(String username, String adminKey) {
+  record AdminRequest(String username, Boolean isAdmin) {
   }
 
   @DeleteMapping("/delete")
@@ -37,14 +34,22 @@ public class AdminController {
     return;
   }
 
-  @PatchMapping
-  public User makeAdminUser(@RequestBody AdminRequest request, HttpServletResponse res) throws IOException {
+  @PatchMapping("/permissions")
+  public User userPermissions(@RequestBody AdminRequest request, HttpServletResponse res) throws IOException {
     User user = userRepo.findByUsername(request.username());
 
     if (user == null) {
       res.sendError(400, "User not found");
       return null;
     }
+
+    if (request.isAdmin() == null) {
+      res.sendError(400, "User privileges not specified");
+      return null;
+    }
+
+    user.setIsAdmin(request.isAdmin());
+    userRepo.save(user);
 
     return user;
   }
--- backend/src/main/java/com/cmpt276project/projectbackend/models/User.java
+++ backend/src/main/java/com/cmpt276project/projectbackend/models/User.java
@@ -60,7 +60,7 @@ public class User {
     this.password = password;
   }
 
-  public void setisAdmin(Boolean isAdmin) {
+  public void setIsAdmin(Boolean isAdmin) {
     this.isAdmin = isAdmin;
   }
 
