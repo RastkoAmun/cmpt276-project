533121c6f43d524fea95ea0c2e79449cd49c9b64
00sea <seanshen12@gmail.com>
2023-07-30 12:19:17 -0700

    header changes based on the current setting page

--- frontend/src/pages/SettingsPages/SettingsMain.jsx
+++ frontend/src/pages/SettingsPages/SettingsMain.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Typography, Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import React, {useState, useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
+import { Typography, Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { Link, Outlet } from 'react-router-dom';
 import { titleContainerStyle } from '../Style';
 
@@ -12,7 +13,44 @@ const list = [
   { title: 'Hydration', path: '/settings/hydration'},
 ]
 
+
+
+
 const SettingsMain = () => {
+
+  const location = useLocation();
+  const [currentHeader, setCurrentHeader] = useState(location.pathname);
+
+  const handleHeaderChange = () => {
+    switch (location.pathname) {
+      case '/settings/profile':
+        setCurrentHeader('Profile');
+        break;
+      case '/settings':
+        setCurrentHeader('General');
+        break;
+      case '/settings/food':
+        setCurrentHeader('Food');
+        break;
+      case '/settings/sleep':
+        setCurrentHeader('Sleep');
+        break;
+      case '/settings/exercise':
+        setCurrentHeader('Exercise');
+        break;
+      case '/settings/hydration':
+        setCurrentHeader('Hydration');
+        break;
+    } 
+  }
+  useEffect(() => {
+    handleHeaderChange();
+  })
+  
+
+
+
+
   
   return (
       <>
@@ -22,7 +60,7 @@ const SettingsMain = () => {
               Settings
             </Typography>
             <Typography variant="fh1"  paddingBottom='20px'>
-              General Settings
+              {currentHeader} Settings
             </Typography>
 
             <Box>
@@ -30,6 +68,7 @@ const SettingsMain = () => {
                 {list.map((item, index) => (
                   <ListItem key={`${item.title}-${index}`} disablePadding>
                     <ListItemButton 
+                      onClick = {handleHeaderChange}
                       component={Link} to={item.path}
                       sx={{ /*borderBottom: '1px solid black ',*/ color: 'light.grey', padding: '8px 16px', }}>
                       <ListItemText primary={<Typography variant='body2' sx={{fontWeight: '400'}}>{item.title}</Typography>} />
