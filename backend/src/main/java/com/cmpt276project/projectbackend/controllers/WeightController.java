58c6f742e1777d9be2b5c2610137d7dafbcd1c1a
Anson Chan <achan436@my.bcit.ca>
2023-07-30 18:56:13 -0700

    feat: weight controller & updated repo functions

---
+++ backend/src/main/java/com/cmpt276project/projectbackend/controllers/WeightController.java
@@ -0,0 +1,59 @@
+package com.cmpt276project.projectbackend.controllers;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.cmpt276project.projectbackend.models.WeightRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.cmpt276project.projectbackend.models.Weight;
+
+@CrossOrigin(origins = { "http://localhost:3000" }, allowedHeaders = "*", allowCredentials = "true")
+@RestController
+@RequestMapping("/weight")
+public class WeightController {
+  @Autowired
+  private WeightRepository weightRepo;
+
+  record WeightRequest(int uid, int weightId, LocalDate date) {
+  }
+
+  record WeightResponseObject(List<Weight> weightHistory) {
+
+  }
+
+  @PostMapping
+  public WeightResponseObject getWeightHistory(@RequestBody WeightRequest request) {
+    List<Weight> weightList = weightRepo.findAllByUid(request.uid());
+    return new WeightResponseObject(weightList);
+  }
+
+  @PostMapping("/add")
+  public Weight addWeight(@RequestBody WeightRequest request, HttpServletResponse res) throws IOException {
+    try {
+      Weight newWeight = new Weight(request.uid(), request.date());
+      weightRepo.save(newWeight);
+      return newWeight;
+    } catch (Exception e) {
+      res.sendError(400, e.getMessage());
+      return null;
+    }
+  }
+
+  @DeleteMapping("/delete")
+  public void deleteWeight(@RequestBody WeightRequest request, HttpServletResponse res) throws IOException {
+    Weight weight = weightRepo.findById(request.weightId()).orElse(null);
+    if (weight == null) {
+      res.sendError(400, "Weight entry with specified ID does not exist");
+      return;
+    }
+
+    weightRepo.delete(weight);
+    res.setStatus(200);
+    return;
+  }
+}
