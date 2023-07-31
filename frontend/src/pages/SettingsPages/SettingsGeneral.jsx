deffe824653169bf451a4d1554f6c914e6bd6643
00sea <seanshen12@gmail.com>
2023-07-31 03:50:37 -0700

    dark mode slider connected to backend

--- frontend/src/pages/SettingsPages/SettingsGeneral.jsx
+++ frontend/src/pages/SettingsPages/SettingsGeneral.jsx
@@ -1,15 +1,71 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, {useState, useEffect, useContext} from 'react'
+import { Typography, Card, Switch} from '@mui/material'
+import axios from 'axios';
+import { UserContext } from '../../index'
+
 
 const SettingsGeneral = () => {
-  return (
+  const { globalUser } = useContext(UserContext);
+
+  const [darkMode, setDarkMode] = useState(true);
+  const [refresh, setRefresh] = useState(0);
+
+
+  const fetchUser = async () => {
+    if (!globalUser) {
+      return;
+    }
+    const res = await axios.post('http://localhost:8080/user/profile', {
+      "uid": globalUser.uid
+    })
+
+    setDarkMode(res.data.darkMode);
+  }
+
+  const handleDarkModeToggle = async () => {
+    if (!globalUser) {
+      return
+    }
+    
+      if (darkMode==true) {
+        setDarkMode(false);
+        await axios.patch('http://localhost:8080/user/darkmode', {
+          "uid": globalUser.uid,
+          "darkmode": false
+        }, {withCredentials: true});
+      }
+      else {
+        setDarkMode(true);
+        await axios.patch('http://localhost:8080/user/darkmode', {
+          "uid": globalUser.uid,
+          "darkmode": true
+        }, {withCredentials: true});
+      }
+
+
+  };
+
+  useEffect(() => {
+    fetchUser();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refresh])
+
+    return (
+
+
     <>
-      <Typography variant='h1'>
-        General settings
-      </Typography>
-      <Typography variant="h2" sx={{ color: 'red' }}>
-        Out of Scope
-      </Typography>
+      <Card>
+        <Typography>
+          Dark Mode {darkMode.toString()}
+        </Typography>
+        <Switch
+          checked={darkMode}
+          onChange={handleDarkModeToggle}
+          color="primary"
+        />
+      </Card>
+
     </>
   )
 }
